ef11a569f01188ce59c98dc3d260a5697ee01262
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-04-12 16:22:18 -0600

    error fix

--- snowshelves_api/urls.py
+++ snowshelves_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', include('locationss_api.urls')),
+    path('', include('locations_api.urls')),
     path('admin/', admin.site.urls),
 ]
