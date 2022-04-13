8f34411c2b9d7777fba246c3321d12cf0be29c21
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-04-13 17:02:58 -0600

    /

--- locations_api/urls.py
+++ locations_api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/locations', views.LocationList.as_view(), name='location_list'),
+    path('/locations/', views.LocationList.as_view(), name='location_list'),
     path('/locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'), 
-    path('/equipment', views.EquipmentList.as_view(), name='equipment_list'),
+    path('/equipment/', views.EquipmentList.as_view(), name='equipment_list'),
     path('/equipment/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'), 
 ]
