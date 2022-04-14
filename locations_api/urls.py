e4ae0985f4c2b674fb37f94d2d5ea30a11c7076b
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-04-14 12:22:50 -0600

    heroku

--- locations_api/urls.py
+++ locations_api/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('locations/', views.LocationList.as_view(), name='location_list'),
     path('locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'), 
     path('equipment/', views.EquipmentList.as_view(), name='equipment_list'),
-    path('equipment/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'), 
+    path('equipment/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'),
+    path('equipment/ski', views.EquipmentList.as_view(), name='equipment_list'),
+    path('equipment/ski/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'), 
+    path('equipment/snowboard', views.EquipmentList.as_view(), name='equipment_list'),
+    path('equipment/snowboard/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'),  
 ]
