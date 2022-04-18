4b8f6b7c99bb870c8c9e12b6d28bace5a6d71095
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-04-17 21:03:14 -0600

    url updates.

--- locations_api/urls.py
+++ locations_api/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'), 
     path('equipment/', views.EquipmentList.as_view(), name='equipment_list'),
     path('equipment/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'),
-    path('equipment/ski', views.EquipmentList.as_view(), name='equipment_list'),
-    path('equipment/ski/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'), 
-    path('equipment/snowboard', views.EquipmentList.as_view(), name='equipment_list'),
-    path('equipment/snowboard/<int:pk>', views.EquipmentDetail.as_view(), name='equipment_detail'),  
 ]
